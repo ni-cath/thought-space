05a6924fac14bc4b741cda87743bc1325fe50afc
Catherine Nikulina <sveklushka@gmail.com>
2025-01-13 01:18:06 +0100

    Basic spring project

---
+++ thought-space-rest-api/src/main/java/com/nicath/website/thought_space_rest_api/ThoughtSpaceRestApiApplication.java
@@ -0,0 +1,12 @@
+package com.nicath.website.thought_space_rest_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThoughtSpaceRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThoughtSpaceRestApiApplication.class, args);
+	}
+}
---
+++ thought-space-rest-api/src/test/java/com/nicath/website/thought_space_rest_api/ThoughtSpaceRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nicath.website.thought_space_rest_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThoughtSpaceRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
